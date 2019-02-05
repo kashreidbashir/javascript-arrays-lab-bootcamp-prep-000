4da1bbe833da240a5600d9fea1cc52179a79da17
Kash Reid-Bashir <kash@reid-bashir.com>
2019-02-05 14:33:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var kittens = ["Milo", "Otis, "Garfield"]
-{
+var kittens = ["Milo", "Otis, "Garfield"];
 
+{
 function destructivelyAppendKitten(name);
 kittens.push(name);
 return kittens
-}
 
 
 function destructivelyApppendKitten() {
@@ -14,33 +13,33 @@ return kittens;
 function destructivelyRemoveLastKitten() {
   kittens.pop();
   return kittens;
-}
+
 
 function destructivelyRemoveFirstKitten() {
   kittens.shift();
   return kittens;
-}
+
 
 function appendKitten(name){
   return [...kittens, name];
-}
+
 
 function prependKitten(name) {
   return [name, ...kittens];
-}
+
 
 function removeLastKitten() {
   return kittens.slice(0, kittens.length - 1);
-}
+
 
 function removeFirstKitten() {
   return kittens.slice(1);
+
 }
 
 
 
-{}
 
-} //define your array here
+ //define your array here
 
 // Add your functions and code here
